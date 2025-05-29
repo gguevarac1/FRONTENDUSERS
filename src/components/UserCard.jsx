888959679aacd0be24721e930f06fb976d315766
gguevara <gguevarac@unemi.edu.ec>
2025-05-29 07:47:05 -0500

    .

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+import { Link } from "react-router";
+
+export const UserCard = () => {
+    return (
+        <div>
+            <h2>{user.name}</h2>
+            <h2>{user.email}</h2>
+        </div>
+    )
+}
